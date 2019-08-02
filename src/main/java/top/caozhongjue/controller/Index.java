f5e3d3017979463343add5f72fa3d637e9a111c9
caozhongjue <2857113935@qq.com>
2019-08-03 00:44:00 +0800

    第二次修改

---
+++ src/main/java/top/caozhongjue/controller/Index.java
@@ -0,0 +1,16 @@
+package top.caozhongjue.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Index {
+    @RequestMapping("/")
+    public String index(@RequestParam(name = "name" )String name , Model model) {
+        model.addAttribute("name",name);
+        return "index";
+    }
+
+}
--- src/test/java/top/caozhongjue/MyblogApplicationTests.java
+++ src/test/java/top/caozhongjue/MyblogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.caozhongjue;
+package top.caozhongjue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
